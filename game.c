c279cdf9a5d01cfbe62e9fca2b387b97b2a5aaa9
Chethan_Bhat <86311185+Chethanbhat04@users.noreply.github.com>
2023-09-04 16:39:28 +0530

    Update game.c

--- game.c
+++ game.c
@@ -7,7 +7,6 @@
 #define NUM_SNAKES 5
 #define NUM_LADDERS 5
 
-
 int snakes[NUM_SNAKES][2] = {
     {16, 6},
     {47, 26},
@@ -24,19 +23,16 @@ int ladders[NUM_LADDERS][2] = {
     {40, 59}
 };
 
-
 int rollDice() {
     return rand() % 6 + 1;
 }
 
-// Function to handle the movement of a player based on the dice roll
 int movePlayer(int currentPlayerPosition, int diceValue) {
     int newPosition = currentPlayerPosition + diceValue;
     if (newPosition > BOARD_SIZE) {
         newPosition = BOARD_SIZE - (newPosition - BOARD_SIZE);
     }
 
-    // Check if the new position has a snake or ladder
     for (int i = 0; i < NUM_SNAKES; i++) {
         if (newPosition == snakes[i][0]) {
             printf("Oops! Snake bites. Going down from %d to %d\n", newPosition, snakes[i][1]);
@@ -56,18 +52,12 @@ int movePlayer(int currentPlayerPosition, int diceValue) {
     return newPosition;
 }
 
-// Function to display the game board with players' positions
 void displayBoard(int playerPositions[]) {
     printf("\n");
-    for (int row = 10; row >= 1; row--) {
-        for (int col = 1; col <= 10; col++) {
-            int position = row * 10;
-            if (row % 2 == 0) {
-                position += 10 - col + 1;
-            } else {
-                position += col;
-            }
+    int position = 100; // Start at position 100
 
+    for (int row = 1; row <= 10; row++) {
+        for (int col = 1; col <= 10; col++) {
             // Check if any player is present at this position
             int playerPresent = 0;
             for (int i = 0; i < NUM_PLAYERS; i++) {
@@ -80,54 +70,50 @@ void displayBoard(int playerPositions[]) {
             if (!playerPresent) {
                 printf("%02d ", position);
             }
+            position--;
         }
         printf("\n");
     }
 }
 
+
+
 int main() {
-    srand(time(NULL));                                                                 // Seed the random number generator with the current time
+    srand(time(NULL));
 
     int playerPositions[NUM_PLAYERS];
     for (int i = 0; i < NUM_PLAYERS; i++) {
-        playerPositions[i] = 1;                                                     // Initialize all players at position 1
+        playerPositions[i] = 1;
     }
 
     printf("Welcome to the Snake and Ladder Game!\n");
 
     int currentPlayerIndex = 0;
     while (1) {
-                                                                                // Display the current board state for all players
         printf("\nCurrent Positions:\n");
         for (int i = 0; i < NUM_PLAYERS; i++) {
             printf("Player %d: %d\n", i + 1, playerPositions[i]);
         }
 
-     // Display the board with player positions
         displayBoard(playerPositions);
 
-        // Player rolls the dice
         printf("\nPlayer %d's Turn:\n", currentPlayerIndex + 1);
         printf("Press Enter to roll the dice...");
-        getchar(); // Wait for the player to press Enter
+        getchar();
         int diceValue = rollDice();
         printf("Player %d rolled a %d\n", currentPlayerIndex + 1, diceValue);
 
-                                                        // Move the player based on the dice roll
         int currentPlayerPosition = playerPositions[currentPlayerIndex];
         int newPosition = movePlayer(currentPlayerPosition, diceValue);
         playerPositions[currentPlayerIndex] = newPosition;
 
-                    
         if (newPosition == BOARD_SIZE) {
             printf("Player %d wins!\n", currentPlayerIndex + 1);
             break;
         }
 
-                     // Move to the next player
         currentPlayerIndex = (currentPlayerIndex + 1) % NUM_PLAYERS;
-
-      
+    }
 
     return 0;
 }
